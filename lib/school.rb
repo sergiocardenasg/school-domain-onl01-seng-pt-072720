a0d2b105ea8e56c7f11baf558c338112679f504d
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-12 03:25:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
 
   def add_student(name, grade)
-    @student_name = name
+    @name = name
     @grade = grade
     if @roster.include?(grade) == false
       @roster[grade] = []
