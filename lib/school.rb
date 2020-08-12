9b4f4508b8f470bfed0cb77f77ed7b1599d6286f
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-12 02:16:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,9 @@
 class School
   attr_accessor :name, :roster
 
-  def initialize(name)
+  def initialize(name, roster)
     @name = name
-  end
-
-  def roster=(roster)
     @roster = {}
   end
+
 end
